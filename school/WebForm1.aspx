e5b02da8807c7f14eec646b675b9b712ed0e4018
ADMIN <ADMIN@ITD-LIS-WS-040>
2024-10-01 16:03:21 +0530

    Add project files.

---
+++ school/WebForm1.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="school.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Details</title>
+</head>
+<body>
+    <form id="Student_Details" runat="server">
+        <div>
+            <center>
+            <table>
+                <tr>
+                    <td>Student Name: </td>
+                    <td><asp:TextBox runat="server" ID="txtname"></asp:TextBox></td>
+                    <td><asp:TextBox runat="server" ID="txtsearch" placeholder="Search"></asp:TextBox></td>
+                    <td><asp:Button runat="server" Text="Search" ID="btnsearch" OnClick="btnsearch_Click" ></asp:Button></td>
+                    <td><asp:Button runat="server" Text="Sorted Data" ID="btnsort" OnClick="btnsort_Click" ></asp:Button></td>
+                </tr>
+                <tr>
+                    <td>Student Subject: </td>
+                    <td><asp:TextBox runat="server" ID="txtsubject"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Student Class: </td>
+                    <td><asp:TextBox runat="server" ID="txtClass"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><asp:Button id="txtsubmit" runat="server" Text="Submit" OnClick="txtsubmit_Click"/></td>
+                </tr>
+               </table>
+                <asp:GridView runat="server" ID="gv" AutoGenerateColumns="False" OnRowCommand="gv_RowCommand" width="100%" style="text-align:center" CellPadding="4" ForeColor="#333333" GridLines="None">
+                    <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+                    <Columns>
+                        <asp:TemplateField HeaderText="STUDENT ID">
+                            <ItemTemplate>
+                                <%#Eval("std_id") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                         <asp:TemplateField HeaderText="STUDENT NAME">
+                            <ItemTemplate>
+                                <%#Eval("std_name") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                         <asp:TemplateField HeaderText="STUDENT SUBJECT">
+                            <ItemTemplate>
+                                <%#Eval("std_sub") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                         <asp:TemplateField HeaderText="STUDENT CLASS">
+                            <ItemTemplate>
+                                   <%#Eval("std_class") %>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                         <asp:TemplateField HeaderText="">
+                            <ItemTemplate>
+                                   <asp:LinkButton ID="btndel" runat="server" Text="Delete" CommandName="del" CommandArgument='<%#Eval("std_id") %>'></asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                         <asp:TemplateField HeaderText="">
+                            <ItemTemplate>
+                                   <asp:LinkButton ID="btnedit" runat="server" Text="Edit" CommandName="thedit" CommandArgument='<%#Eval("std_id") %>'></asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                    <EditRowStyle BackColor="#999999" />
+                    <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+                    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                    <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                    <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                    <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                    <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                    <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                    <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+                </asp:GridView>
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ school/WebForm1.aspx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace school
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source=ITD-LIS-WS-040\\SQLEXPRESS; initial catalog=school; integrated security=true;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            display();
+
+        }
+
+        protected void txtsubmit_Click(object sender, EventArgs e)
+        {
+            if(txtsubmit.Text == "Submit")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("InsertStudentDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@std_name", txtname.Text);
+                cmd.Parameters.AddWithValue("@std_sub", txtsubject.Text);
+                cmd.Parameters.AddWithValue("@std_class", txtClass.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                display();
+            }
+            else if (txtsubmit.Text == "Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UpdateStudentDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@std_id", ViewState["NewId"]);
+                cmd.Parameters.AddWithValue("@std_name", txtname.Text);
+                cmd.Parameters.AddWithValue("@std_sub", txtsubject.Text);
+                cmd.Parameters.AddWithValue("@std_class", txtClass.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                display();
+            }
+        }
+
+        public void display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DisplayStudentDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            gv.DataSource = dt;
+            gv.DataBind();
+        }
+
+        protected void gv_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "del")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DeleteStudentDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@std_id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                display();
+
+            }
+                else if (e.CommandName == "thedit")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("EditStudentDetails", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue ("@std_id", e.CommandArgument);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0][1].ToString();
+                txtsubject.Text = dt.Rows[0][2].ToString();
+                txtClass.Text = dt.Rows[0][3].ToString();
+                txtsubmit.Text = "Update";
+                ViewState["NewId"] = e.CommandArgument;
+            }
+        }
+
+        protected void btnsearch_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SearchStudentDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@std_name", txtsearch.Text);
+            cmd.Parameters.AddWithValue("@std_sub", txtsearch.Text);
+            cmd.Parameters.AddWithValue("@std_class", txtsearch.Text);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            gv.DataSource = dt;
+            gv.DataBind();
+        }
+
+        protected void btnsort_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SortStudentDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@std_class",txtClass.Text );
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            gv.DataSource = dt;
+            gv.DataBind();
+        }
+    }
+}
